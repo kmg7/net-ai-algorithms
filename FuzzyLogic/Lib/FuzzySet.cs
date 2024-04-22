de978dc485fa22014add61a4318515b487b9f9d5
kmg7 <kmlgkcy.dev@gmail.com>
2024-04-22 11:59:35 +0300

    refactored MemberIO and Function to respect the Literature

---
+++ FuzzyLogic/Lib/FuzzySet.cs
@@ -0,0 +1,17 @@
+﻿namespace Lib
+{
+    internal class FuzzySet
+    {
+        public string Name { get; set; }
+        public FunctionType FunctionType { get; set; }
+        public double[] Parameters { get; set; }
+
+        public FuzzySet(string name, double[] parameters, FunctionType functionType)
+        {
+            // Error handling is still under consideration
+            Name = name;
+            Parameters = parameters;
+            FunctionType = functionType;
+        }
+    }
+}
---
+++ FuzzyLogic/Lib/FuzzyVariable.cs
@@ -0,0 +1,17 @@
+﻿namespace Lib
+{
+    internal class FuzzyVariable
+    {
+        public string Name { get; set; }
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+        public List<FuzzySet> Sets { get; set; }
+        public FuzzyVariable(string name, double min, double max, List<FuzzySet> members)
+        {
+            Name = name;
+            MinValue = min;
+            MaxValue = max;
+            Sets = members;
+        }
+    }
+}
