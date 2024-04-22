9885a7e2ba683f86fa0cce558113b88f4741e24a
kmg7 <kmlgkcy.dev@gmail.com>
2024-04-22 12:04:00 +0300

    FuzzyRule added

---
+++ FuzzyLogic/Lib/FuzzyRule.cs
@@ -0,0 +1,14 @@
+﻿namespace Lib
+{
+    internal class FuzzyRule
+    {
+        public FuzzySet[] Antecedents { get; set; }
+        public FuzzySet[] Consequents { get; set; }
+
+        public FuzzyRule(FuzzySet[] antecedents, FuzzySet[] consequents)
+        {
+            Antecedents = antecedents;
+            Consequents = consequents;
+        }
+    }
+}
